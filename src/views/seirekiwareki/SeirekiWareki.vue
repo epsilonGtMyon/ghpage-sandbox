2ab983e510ea2aecd69e59c67a7006003c5857c6
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2021-08-15 11:57:03 +0900

    cardカスタマイズ

--- src/views/seirekiwareki/SeirekiWareki.vue
+++ src/views/seirekiwareki/SeirekiWareki.vue
@@ -1,51 +1,61 @@
 <template>
   <div class="seirekiWareki px-2">
-    <div class="card px-4 py-4">
-      <div class="columns is-vcentered is-multiline">
-        <div class="column is-12">
-          <div class="field">
-            <label class="label">西暦</label>
-            <div class="is-flex is-align-items-center">
-              <input
-                class="input has-text-right"
-                type="text"
-                style="width: 5em"
-                v-model="year"
-              />
-              年
-              <div class="select">
-                <select v-model="month">
-                  <template v-for="x of 12" :key="x">
-                    <option :value="x - 1">{{ x }}</option>
-                  </template>
-                </select>
+    <div class="card">
+      <header class="card-header is-primary">
+        <p class="card-header-title">求めたい西暦</p>
+      </header>
+      <div class="card-content">
+        <div class="columns is-vcentered is-multiline">
+          <div class="column is-12">
+            <div class="field">
+              <label class="label">西暦</label>
+              <div class="is-flex is-align-items-center">
+                <input
+                  class="input has-text-right"
+                  type="text"
+                  style="width: 5em"
+                  v-model="year"
+                />
+                年
+                <div class="select">
+                  <select v-model="month">
+                    <template v-for="x of 12" :key="x">
+                      <option :value="x - 1">{{ x }}</option>
+                    </template>
+                  </select>
+                </div>
+                月
+                <div class="select">
+                  <select v-model="date">
+                    <template v-for="x of 31" :key="x">
+                      <option :value="x">{{ x }}</option>
+                    </template>
+                  </select>
+                </div>
+                日
               </div>
-              月
-              <div class="select">
-                <select v-model="date">
-                  <template v-for="x of 31" :key="x">
-                    <option :value="x">{{ x }}</option>
-                  </template>
-                </select>
-              </div>
-              日
             </div>
           </div>
-        </div>
-        <div class="column is-12">
-          <button class="button is-primary" @click="resolve">決定</button>
+          <div class="column is-12">
+            <button class="button is-primary" @click="resolve">決定</button>
+          </div>
         </div>
       </div>
     </div>
 
-    <div class="card px-4 py-4 mt-4">
-      {{ jsDate.toLocaleDateString() }}
+    <div class="card mt-4">
+      <header class="card-header is-primary">
+        <p class="card-header-title">結果</p>
+      </header>
+      <div class="card-content">
+        {{ jsDate.toLocaleDateString() }}
 
-      <ul>
-        <template v-for="(text, index) of warekiTexts" :key="index">
-          <li>{{ text }}</li>
-        </template>
-      </ul>
+        <ul>
+          <template v-for="(text, index) of warekiTexts" :key="index">
+            <li>{{ text }}</li>
+          </template>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
